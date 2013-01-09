47787820a82f1cbeec2c3d76fd88eed2c5a97c99
Joe Daughtery <jdaughtery@t-sciences.com>
2013-01-09 15:36:21 -0600

    Updated configuration and code to use the new configuration

--- src/ozone/securitysample/authentication/ldap/LdapUserDetailsService.java
+++ src/ozone/securitysample/authentication/ldap/LdapUserDetailsService.java
@@ -15,130 +15,93 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import ozone.security.authentication.OWFUserDetailsImpl;
 
 public class LdapUserDetailsService
-  implements UserDetailsService
-{
-  private static final Log log = LogFactory.getLog(LdapUserDetailsService.class);
-  private LdapOperations ldapOperations;
-  private String roleSearchBase;
-  private String roleSearchQuery;
-  private String groupSearchBase;
-  private String groupSearchQuery;
+        implements UserDetailsService {
 
-  public UserDetails loadUserByUsername(String certificateUserInfo)
-    throws UsernameNotFoundException, DataAccessException
-  {
-    log.debug("loading user by username [" + certificateUserInfo + "]");
+    private static final Log log = LogFactory.getLog(LdapUserDetailsService.class);
+    private LdapOperations ldapOperations;
+    private String roleSearchBase;
+    private String roleSearchQuery;
+    private String groupSearchBase;
+    private String groupSearchQuery;
 
-    UserDetails userDetails = null;
-
-    String preparedRoleSearchQuery = this.roleSearchQuery.replace("?", certificateUserInfo);
-    String preparedGroupSearchQuery = this.groupSearchQuery.replace("?", certificateUserInfo);
-
-    log.debug("Prepared role search query is " + preparedRoleSearchQuery + " and preparedGroupSearchQuery is " + preparedGroupSearchQuery);
-    try
-    {
-      ///  
-        AndFilter andFilterDN = new AndFilter();
-        andFilterDN.and(new EqualsFilter("objectClass","user"));
-        andFilterDN.and(new EqualsFilter("sAMAccountName",certificateUserInfo));
+    public UserDetails loadUserByUsername(String certificateUserInfo)
+            throws UsernameNotFoundException, DataAccessException {
         
-        AndFilter andFilterRole = new AndFilter();
-        andFilterRole.and(new EqualsFilter("objectClass","group"));
-        //andFilterRole.and(new EqualsFilter("sAMAccountName",certificateUserInfo));
-        
-        AndFilter andFilterGroup = new AndFilter();
-        andFilterGroup.and(new EqualsFilter("objectClass","group"));
-        
-        System.out.println("LDAP Query " + andFilterRole.encode());
-        List userRoles = null;
-        List userGroups = null;
-        String dn = null;
+        UserDetails userDetails = null;
         try {
-            userRoles = ldapOperations.search("", andFilterDN.encode(),new LdapAuthorityGroupContextMapper());
-            log.debug("search returned [" + (userRoles != null ? userRoles.size() : 0) + "] role(s)");
-            LdapAuthorityGroup lag = (LdapAuthorityGroup)userRoles.get(0);
-            dn = lag.getDn();
-            //String dn1 = lag.getDn()+",DC=dev,DC=wisrd,DC=org";
-            String dn1 = lag.getDn() + lag.getDnBase();
-            andFilterRole.and(new EqualsFilter("member",dn1));
-            userRoles.clear();
-            userRoles = ldapOperations.search("", andFilterRole.encode(),new LdapAuthorityGroupContextMapper());
-            
-            LdapAuthorityGroup lag2 = (LdapAuthorityGroup)userRoles.get(0);
-            //String dn2 = lag.getDn()+",DC=dev,DC=wisrd,DC=org";
-            andFilterGroup.and(new EqualsFilter("member",dn1));
-            userGroups = ldapOperations.search("", andFilterGroup.encode(),new LdapAuthorityGroupContextMapper());
-            log.debug("search returned [" + (userGroups != null ? userGroups.size() : 0) + "] group(s)");
-        } catch(PartialResultException pre) {
-            //Not sure why this is thrown when we locate the data.
+
+            AndFilter andFilterDN = new AndFilter();
+            andFilterDN.and(new EqualsFilter("objectClass", "user"));
+            andFilterDN.and(new EqualsFilter("sAMAccountName", certificateUserInfo));
+
+            List userDN = ldapOperations.search("", andFilterDN.encode(), new LdapAuthorityGroupContextMapper());
+            if (userDN.size() > 0) {
+                LdapAuthorityGroup lag = (LdapAuthorityGroup) userDN.get(0);
+                String dn = lag.getDn();
+                String dn1 = lag.getDn() + lag.getDnBase();
+                userDN.clear();
+
+
+                String preparedRoleSearchQuery = this.roleSearchQuery.replace("?", dn1);
+                String preparedGroupSearchQuery = this.groupSearchQuery.replace("?", dn1);
+                List userRoles = this.ldapOperations.search("", preparedRoleSearchQuery, new LdapAuthorityGroupContextMapper());
+                log.debug("search returned [" + (userRoles != null ? userRoles.size() : 0) + "] role(s)");
+
+                List userGroups = this.ldapOperations.search("", preparedGroupSearchQuery, new LdapAuthorityGroupContextMapper());
+                log.debug("search returned [" + (userGroups != null ? userGroups.size() : 0) + "] group(s)");
+
+                userDetails = (UserDetails) this.ldapOperations.lookup(dn, new LdapUserDetailsContextMapper(userRoles, userGroups));
+            }
+            if (userDetails == null) {
+                log.error("No results found for [" + certificateUserInfo + "].");
+                throw new UsernameNotFoundException("The user details service was passed authenticated credentials for '" + certificateUserInfo + "', but the credentials were not found. Access to the Ozone Widgeting Framework is denied.");
+            }
+
+            if (userDetails.getAuthorities().size() == 0) {
+                log.error("No authorities found for [" + certificateUserInfo + "].");
+                throw new UsernameNotFoundException("The user details service was passed authenticated credentials for '" + certificateUserInfo + "', but found no authorities assigned. Access to the Ozone Widgeting Framework is denied.");
+            }
+        } catch (CommunicationException cex) {
+            log.error("Communication exception.", cex);
+            throw new UsernameNotFoundException("Unable to connect to directory service. Access to the Ozone Widgeting Framework is denied.");
+        } catch (Exception ex) {
+            log.error("Other exception.", ex);
+            throw new UsernameNotFoundException("Unable to process data from directory service. Access to the Ozone Widgeting Framework is denied.");
         }
-      ///  
-      /*List userRoles = this.ldapOperations.search(this.roleSearchBase, preparedRoleSearchQuery, new LdapAuthorityGroupContextMapper());
-      log.debug("search returned [" + (userRoles != null ? userRoles.size() : 0) + "] role(s)");
 
-      List userGroups = this.ldapOperations.search(this.groupSearchBase, preparedGroupSearchQuery, new LdapAuthorityGroupContextMapper());
-      log.debug("search returned [" + (userGroups != null ? userGroups.size() : 0) + "] group(s)");*/
-      if(userRoles.size() > 0 || userGroups.size() > 0) {
-          LdapAuthorityGroup lag = (LdapAuthorityGroup)userRoles.get(0);
-          //String dn = lag.getDn()+",DC=dev,DC=wisrd,DC=org";
-          //String dn = lag.getDn();
-      //userDetails = (UserDetails)this.ldapOperations.lookup(certificateUserInfo, new LdapUserDetailsContextMapper(userRoles, userGroups));
-          userDetails = (UserDetails)this.ldapOperations.lookup(dn, new LdapUserDetailsContextMapper(userRoles, userGroups));
-      }
-
-      if (userDetails == null) {
-        log.error("No results found for [" + certificateUserInfo + "].");
-        throw new UsernameNotFoundException("The user details service was passed authenticated credentials for '" + certificateUserInfo + "', but the credentials were not found. Access to the Ozone Widgeting Framework is denied.");
-      }
-
-      if (userDetails.getAuthorities().size() == 0) {
-        log.error("No authorities found for [" + certificateUserInfo + "].");
-        throw new UsernameNotFoundException("The user details service was passed authenticated credentials for '" + certificateUserInfo + "', but found no authorities assigned. Access to the Ozone Widgeting Framework is denied.");
-      }
-    }
-    catch (CommunicationException cex)
-    {
-      log.error("Communication exception.", cex);
-      throw new UsernameNotFoundException("Unable to connect to directory service. Access to the Ozone Widgeting Framework is denied.");
-    } catch (Exception ex) {
-      log.error("Other exception.", ex);
-      throw new UsernameNotFoundException("Unable to process data from directory service. Access to the Ozone Widgeting Framework is denied.");
+        log.info("successfully logged in user [" + userDetails.getUsername() + "], authorities [" + ((OWFUserDetailsImpl) userDetails).displayAuthorities() + "], groups [" + ((OWFUserDetailsImpl) userDetails).displayOwfGroups() + "]");
+        return userDetails;
     }
 
-    log.info("successfully logged in user [" + userDetails.getUsername() + "], authorities [" + ((OWFUserDetailsImpl)userDetails).displayAuthorities() + "], groups [" + ((OWFUserDetailsImpl)userDetails).displayOwfGroups() + "]");
-    return userDetails;
-  }
+    public void setLdapOperations(LdapOperations ldapOperations) {
+        this.ldapOperations = ldapOperations;
+    }
 
-  public void setLdapOperations(LdapOperations ldapOperations)
-  {
-    this.ldapOperations = ldapOperations;
-  }
+    public void setGroupSearchBase(String groupSearchBase) {
+        this.groupSearchBase = groupSearchBase;
+    }
 
-  public void setGroupSearchBase(String groupSearchBase) {
-    this.groupSearchBase = groupSearchBase;
-  }
+    public void setGroupSearchQuery(String groupSearchQuery) {
+        this.groupSearchQuery = groupSearchQuery;
+    }
 
-  public void setGroupSearchQuery(String groupSearchQuery) {
-    this.groupSearchQuery = groupSearchQuery;
-  }
+    public String getRoleSearchBase() {
+        return this.roleSearchBase;
+    }
 
-  public String getRoleSearchBase() {
-    return this.roleSearchBase;
-  }
+    public void setRoleSearchBase(String roleSearchBase) {
+        this.roleSearchBase = roleSearchBase;
+    }
 
-  public void setRoleSearchBase(String roleSearchBase) {
-    this.roleSearchBase = roleSearchBase;
-  }
+    public void setRoleSearchQuery(String roleSearchQuery) {
+        this.roleSearchQuery = roleSearchQuery;
+    }
 
-  public void setRoleSearchQuery(String roleSearchQuery) {
-    this.roleSearchQuery = roleSearchQuery;
-  }
+    public LdapOperations getLdapOperations() {
+        return this.ldapOperations;
+    }
 
-  public LdapOperations getLdapOperations() {
-    return this.ldapOperations;
-  }
-
-  public String getGroupSearchBase() {
-    return this.groupSearchBase;
-  }
+    public String getGroupSearchBase() {
+        return this.groupSearchBase;
+    }
 }
